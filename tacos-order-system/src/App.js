5401148a37ba40cb95035e1b51bb4a99b98aeef5
wjb108 <williambenedict3@gmail.com>
2021-03-27 14:48:21 -0400

    added divs and classnames for tacos image and container

--- tacos-order-system/src/App.js
+++ tacos-order-system/src/App.js
@@ -1,22 +1,21 @@
-import axios from 'axios';
-import './App.css';
-import {baseURL, config} from "./services/index"
-import {useEffect, useState} from "react"
-import TacosList from "./components/TacosList"
-import {Navbar} from "./components/Navbar"
-import { Route } from "react-router-dom"
+import axios from "axios";
+import "./App.css";
+import { baseURL, config } from "./services/index";
+import { useEffect, useState } from "react";
+import TacosList from "./components/TacosList";
+import { Navbar } from "./components/Navbar";
+import { Route } from "react-router-dom";
 
 function App() {
-
-  const [tacos, setTacos] =useState([])
+  const [tacos, setTacos] = useState([]);
 
   useEffect(() => {
-    getTacoData()
-  }, [])
+    getTacoData();
+  }, []);
 
   async function getTacoData() {
-    let response = await axios.get(baseURL, config)
-    setTacos(response.data.records)
+    let response = await axios.get(baseURL, config);
+    setTacos(response.data.records);
     console.log(response.data.records);
   }
   return (
@@ -24,16 +23,18 @@ function App() {
       <Route exact path="/"></Route>
       {tacos.map((taco) => {
         return (
-        <div key={taco.id} id={taco.id}>
-            <img src={taco.fields.image} />
-            <br></br>
-            {taco.fields.title}
-            <br></br>
-            {taco.fields.price}
-            <br></br>
-          {taco.fields.description}
-        </div>
-        )
+          <div className="taco-container" key={taco.id} id={taco.id}>
+            <div className="taco-image-container">
+              <img className="taco-image" src={taco.fields.image} />
+              <br></br>
+              {taco.fields.title}
+              <br></br>
+              {taco.fields.price}
+              <br></br>
+              {taco.fields.description}
+            </div>
+          </div>
+        );
       })}
       {/* <TacosList></TacosList> */}
     </div>
