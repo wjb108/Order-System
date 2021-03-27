72d5aa595a117955711aa112a5477d5904da2dd8
wjb108 <williambenedict3@gmail.com>
2021-03-27 10:41:41 -0400

    api airtable call rendering

--- tacos-order-system/src/App.js
+++ tacos-order-system/src/App.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
 import './App.css';
 import {baseURL, config} from "./services/index"
-import {useEffect} from "react"
+import {useEffect, useState} from "react"
 import TacosList from "./components/TacosList"
+import {Navbar} from "./components/Navbar"
+import { Route } from "react-router-dom"
 
 function App() {
+
+  const [tacos, setTacos] =useState([])
 
   useEffect(() => {
     getTacoData()
@@ -12,11 +16,22 @@ function App() {
 
   async function getTacoData() {
     let response = await axios.get(baseURL, config)
+    setTacos(response.data.records)
     console.log(response.data.records);
   }
   return (
     <div className="App">
-  
+      <Route exact path="/"></Route>
+      {tacos.map((taco) => {
+        return (
+        <div key={taco.id} id={taco.id}>
+          {taco.fields.title}
+          {taco.fields.price}
+          {taco.fields.description}
+        </div>
+        )
+      })}
+      {/* <TacosList></TacosList> */}
     </div>
   );
 }
--- tacos-order-system/src/components/TacosList.jsx
+++ tacos-order-system/src/components/TacosList.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function TacosList() {
-  return <div>TacosList</div>;
+  return (
+    <div>
+      <Link to="/">TacosList</Link>
+    </div>
+  );
 }
