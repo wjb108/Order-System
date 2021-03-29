5d39b062f9c31ef0f29c7758c3121fc8dc1e7537
wjb108 <williambenedict3@gmail.com>
2021-03-29 13:27:37 -0400

    quantity updating

--- tacos-order-system/src/App.js
+++ tacos-order-system/src/App.js
@@ -5,10 +5,12 @@ import { useEffect, useState } from "react";
 import TacosList from "./components/TacosList";
 import Navbar from "./components/Navbar.jsx"
 import { Route } from "react-router-dom";
+import TacoOrderCard from "./components/TacoOrderCard";
 
 
 function App() {
   const [tacos, setTacos] = useState([]);
+  const [cart, setCart] = useState({});
   const [count, setCount] = useState(0);
 
   useEffect(() => {
@@ -20,18 +22,27 @@ function App() {
     setTacos(response.data.records);
     console.log(response.data.records);
   }
-  function handleClick(event) {
+  function handleClick(taco) {
     // console.log(event.target)
     // console.log(setCount(event.target + 1))
     // setCount(event.target.value + 1)
     setCount((prevState) => {
       return prevState + 1
     })
+    setCart((prevState) => {
+      const prevTaco = prevState[taco.id]? { ...prevState[taco.id] } : { ...taco, quantity: 0 }
+      prevTaco.quantity++ 
+      // return { ...prevState, [taco.id]: {...taco, quantity: prevTaco? prevTaco.quantity +1 : 1 } }
+      return { ...prevState, [taco.id]: prevTaco }
+    })
   }
   return (
     <div className="App">
       <Route>
         <Navbar count={count}/>
+      </Route>
+      <Route exact path="/cart">
+        <TacoOrderCard orderTacos={cart}/>
       </Route>
       <Route exact path="/">
         {tacos.map((taco) => {
@@ -44,8 +55,13 @@ function App() {
                 <br></br>
                 {taco.fields.price}
                 <br></br>
+                {taco.fields.quantity}
+                <br></br>
                 {taco.fields.description}
-                <button onClick={handleClick} type="submit">Add To Cart</button>
+                <button onClick={() => handleClick(taco)} type="submit">Add To Cart</button>
+                {/* {cart.filter((item) => {
+                  return item.id === taco.id
+                }).length} */}
               </div>
             </div>
           );
@@ -53,7 +69,7 @@ function App() {
       </Route>
       {/* <TacosList></TacosList> */}
     </div>
-  );
+  )
 }
 
 export default App;
--- tacos-order-system/src/components/TacoOrderCard.jsx
+++ tacos-order-system/src/components/TacoOrderCard.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function TacoOrderCard() {
-  return <div>TacoOrderCard</div>;
+export default function TacoOrderCard(props) {
+  return (
+    <div className="cart-container">
+      <Link to="/new"></Link>
+      {props.orderTacos.map((taco) => {
+        return (
+          <div className="taco-container" key={taco.id}>
+            <div className="taco-image-container">
+              <img className="taco-image" src={taco.fields.image} alt="taco" />
+              <br></br>
+              {taco.fields.title}
+              <br></br>
+              {taco.fields.price}
+              <br></br>
+              {taco.fields.quantity}
+              <br></br>
+              {taco.fields.description}
+              {/* <button onClick={() => handleClick(taco)} type="submit"> */}
+              {/* Add To Cart
+              </button> */}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
