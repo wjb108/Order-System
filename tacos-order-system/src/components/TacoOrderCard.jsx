5ed13fce6fff946405d94af6c24281b17e467932
wjb108 <williambenedict3@gmail.com>
2021-03-30 12:30:48 -0400

    submit button and math sortof working

--- tacos-order-system/src/components/TacoOrderCard.jsx
+++ tacos-order-system/src/components/TacoOrderCard.jsx
@@ -2,35 +2,50 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function TacoOrderCard({ orderTacos }) {
-  // console.log(Object.keys(orderTacos));
+  console.log(Object.keys(orderTacos));
   return (
-    <div className="order-tacos-container">
-      {Object.keys(orderTacos).map((key) => {
-        const taco = orderTacos[key].fields;
-        return (
-          <div className="order-taco-container">
-            <div className="order-taco-image-container">
-              <img className="order-taco-image" src={taco.image} alt="taco" />
-            </div>
-            <div className="order-taco-text-container">
-              <div className="order-taco-text">
-                <br></br>
-                <div className="order-taco-text-title">
-                  {`Name: ${taco.title}`}
-                </div>
-                <br></br>
-                <div className="order-taco-text-quantity">
-                  {`Quantity: ${orderTacos[key].quantity}`}
-                </div>
-                <br></br>
-                <div className="order-taco-text-price">
-                  {`Price: $${taco.price}.00`}
+    <div className="order-page-container">
+      <div className="order-taco-submit-container">
+        <h2>Order Total: </h2>
+        {Object.keys(orderTacos).map((key) => {
+          const quantity = orderTacos[key].quantity;
+          const price = orderTacos[key].fields.price;
+          return <div>{quantity * Number(price)}</div>;
+        })}
+        <div className="order-taco-submit-button">
+          <button className="order-taco-submit-inner-button" type="submit">
+            Submit Order
+          </button>
+        </div>
+      </div>
+      <div className="order-tacos-container">
+        {Object.keys(orderTacos).map((key) => {
+          const taco = orderTacos[key].fields;
+          return (
+            <div className="order-taco-container">
+              <div className="order-taco-image-container">
+                <img className="order-taco-image" src={taco.image} alt="taco" />
+              </div>
+              <div className="order-taco-text-container">
+                <div className="order-taco-text">
+                  <br></br>
+                  <div className="order-taco-text-title">
+                    {`Name: ${taco.title}`}
+                  </div>
+                  <br></br>
+                  <div className="order-taco-text-quantity">
+                    {`Quantity: ${orderTacos[key].quantity}`}
+                  </div>
+                  <br></br>
+                  <div className="order-taco-text-price">
+                    {`Subtotal: $${taco.price * orderTacos[key].quantity}.00`}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 }
