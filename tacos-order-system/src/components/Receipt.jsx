95b98ea84752c5519ca3cbc4397051b9da766120
wjb108 <williambenedict3@gmail.com>
2021-03-31 11:16:17 -0400

    receipt outline working

--- tacos-order-system/src/components/Receipt.jsx
+++ tacos-order-system/src/components/Receipt.jsx
@@ -18,35 +18,8 @@ export default function Receipt() {
 
   return (
     <div className="receipt-cart-container">
-      <Link to="/receipt">hello</Link>
-      <div className="order-tacos-container">
-        {Object.keys(orderNumber).map((key) => {
-          const taco = orderNumber[key].fields;
-          return (
-            <div className="order-taco-container">
-              <div className="order-taco-image-container">
-                <img className="order-taco-image" src={taco.image} alt="taco" />
-              </div>
-              <div className="order-taco-text-container">
-                <div className="order-taco-text">
-                  <br></br>
-                  <div className="order-taco-text-title">
-                    {`Name: ${taco.title}`}
-                  </div>
-                  <br></br>
-                  <div className="order-taco-text-quantity">
-                    {`Quantity: ${orderNumber[key].quantity}`}
-                  </div>
-                  <br></br>
-                  <div className="order-taco-text-price">
-                    {`Subtotal: $${taco.price * orderNumber[key].quantity}.00`}
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+      <Link to="/receipt"></Link>
+      <div className="receipt-tacos-container">hello</div>
     </div>
   );
 }
