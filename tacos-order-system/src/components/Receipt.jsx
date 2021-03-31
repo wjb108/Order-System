8850f3ced9611352ab0d8983ec55b3846c5fff67
wjb108 <williambenedict3@gmail.com>
2021-03-31 12:47:31 -0400

    receipt rendering

--- tacos-order-system/src/components/Receipt.jsx
+++ tacos-order-system/src/components/Receipt.jsx
@@ -19,7 +19,62 @@ export default function Receipt() {
   return (
     <div className="receipt-cart-container">
       <Link to="/receipt"></Link>
-      <div className="receipt-tacos-container">hello</div>
+      <div className="receipt-tacos-container">
+        {Object.keys(orderNumber).map((key) => {
+          const orderId = orderNumber[key].id;
+          console.log(orderId);
+          const orderInfo = orderNumber[key].fields.orderid;
+          const orderInfoString = JSON.parse(orderInfo).orderTacos;
+          console.log(orderInfoString);
+          return (
+            <div className="order-taco-container">
+              <div className="order-taco-text-container">
+                <div className="order-taco-text">
+                  <br></br>
+                  <div className="order-taco-text-title">
+                    {`Order Id: ${orderId}`}
+                  </div>
+                  <br></br>
+                  <div className="order-tacos-container">
+                    {Object.keys(orderInfoString).map((key) => {
+                      const taco = orderInfoString[key].fields;
+                      return (
+                        <div className="order-taco-container">
+                          <div className="order-taco-image-container">
+                            <img
+                              className="order-taco-image"
+                              src={taco.image}
+                              alt="taco"
+                            />
+                          </div>
+                          <div className="order-taco-text-container">
+                            <div className="order-taco-text">
+                              <br></br>
+                              <div className="order-taco-text-title">
+                                {`Name: ${taco.title}`}
+                              </div>
+                              <br></br>
+                              <div className="order-taco-text-quantity">
+                                {`Quantity: ${orderInfoString[key].quantity}`}
+                              </div>
+                              <br></br>
+                              <div className="order-taco-text-price">
+                                {`Subtotal: $${
+                                  taco.price * orderInfoString[key].quantity
+                                }.00`}
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
